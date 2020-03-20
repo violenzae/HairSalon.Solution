e24c97dfaf5d370ddc2b982603f72e22b7c4a140
Rachel Schieferstein <violenza@gmail.com>
2020-03-20 10:13:06 -0700

    adding client create view and get/post routes in controller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -7,11 +7,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Controllers
 {
-  public class ClientController : Controller
+  public class ClientsController : Controller
   {
     private readonly HairSalonContext _db;
 
-    public ClientController(HairSalonContext db)
+    public ClientsController(HairSalonContext db)
     {
       _db = db;
     }
@@ -21,6 +21,20 @@ namespace HairSalon.Controllers
       List<Client> model = _db.Clients.Include(clients => clients.Stylist).ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
 
   }
 }
