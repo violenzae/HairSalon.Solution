cbed8fd9c6aef40d7bb7b2141078588f1a70ace7
Rachel Schieferstein <violenza@gmail.com>
2020-03-20 10:49:23 -0700

    adding client details controller and view

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -34,7 +34,12 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      return View(thisClient);
+    }
+ 
 
   }
 }
