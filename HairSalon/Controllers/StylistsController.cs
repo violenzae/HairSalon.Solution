f6fc6e1f2a3f57c150b219cc2f8257e77d35189d
Rachel Schieferstein <violenza@gmail.com>
2020-03-20 11:32:33 -0700

    adding delete view and routes for stylists/child clients

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -51,5 +51,27 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Delete(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylists => stylists.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Stylist stylistToDelete = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      stylistToDelete.Clients = _db.Clients.Where(client => client.StylistId == id).ToList();
+      foreach(Client client in stylistToDelete.Clients)
+      {
+        _db.Clients.Remove(client);
+      }
+      _db.SaveChanges();
+
+      var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      _db.Stylists.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   } 
 }
