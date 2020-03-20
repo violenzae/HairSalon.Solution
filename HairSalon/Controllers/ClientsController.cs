c954bf19c3da8a558191fb9ceb82ac6965b3b462
Rachel Schieferstein <violenza@gmail.com>
2020-03-20 11:05:45 -0700

    adding client delete routes and view

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -55,7 +55,19 @@ namespace HairSalon.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Delete(int id)
+    {
+        var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+        return View(thisClient);
+    }
 
-
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var thisClient = _db.Clients.FirstOrDefault(Clients => Clients.ClientId == id);
+        _db.Clients.Remove(thisClient);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
   }
 }
